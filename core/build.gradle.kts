f146abb3001e62014a4b83242bfa9bb7917051ef
f4biofish <fabiofishfutebol@gmail.com>
2026-05-31 20:23:42 -0300

    Configurada a implementação do Hilt

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+
 }
 
 android {
@@ -60,4 +62,8 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 }
