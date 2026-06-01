f5fdfcb3af0bb4b61f0ed35b6d4b401830cc7a66
f4biofish <fabiofishfutebol@gmail.com>
2026-06-01 19:45:46 -0300

    Criado o Dao de acesso a dados de Trades

---
+++ core/src/main/java/com/librarys/ferreira/core/data/local/dao/TradesDao.kt
@@ -0,0 +1,29 @@
+package com.librarys.ferreira.core.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.librarys.ferreira.core.data.local.entities.TradesEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TradesDao {
+
+    @Query("SELECT * FROM trades")
+    fun getAllTrades() : Flow<List<TradesEntity>>
+
+    @Query("SELECT * FROM trades WHERE accountInfoId = :accountId")
+    fun getAllTradesByAccountId(accountId: String) : Flow<List<TradesEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertTrades(trades: TradesEntity) : Long
+
+    @Delete
+    fun deleteTrade(trade: TradesEntity) : Long
+
+    @Query("DELETE FROM trades WHERE accountInfoId = :accountId")
+    fun deleteAllTradesByAccount(accountId: String) : Long
+
+}
